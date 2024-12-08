eeef8934c4e8f2e822f94979a54b7965fffde3ca
Hadia Najeeb <132043643+Hadia2003@users.noreply.github.com>
2024-12-08 10:53:32 +0500

    Time: 16 ms (19.10%) | Memory: 49.8 MB (72.46%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+         Map<Integer, Integer> countMap = new HashMap<>();
+        int majorityCount = nums.length / 2;
+
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+
+            if (countMap.get(num) > majorityCount) {
+                return num;
+            }
+        }
+
+        throw new IllegalArgumentException("No majority element found");
+    }
+        
+    }
